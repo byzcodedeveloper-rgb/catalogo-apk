6a05c2ef15398327bd8f6f5aa7f40da3cb84edbe
byzcode <byzcode.developer@gmail.com>
2026-02-05 14:31:25 -0500

    Actualizacion de la vista 21

--- www/js/app.js
+++ www/js/app.js
@@ -48,7 +48,7 @@ $(document).on("click", ".btn-detalle", function(e) {
       // Modal video
       '<div id="video-modal" style="display:none; position:fixed; top:0; left:0; width:100%; height:100%; background:rgba(0,0,0,0.8); justify-content:center; align-items:center; z-index:9999;">' +
         '<video id="video-player" controls style="max-width:90%; max-height:90%; border-radius:10px; box-shadow:0 4px 12px rgba(0,0,0,0.5); background:#000;">' +
-          '<source src="https://www.mediafire.com/file/7qr9q2bn7t1hdti/video' + producto.codigo + '.mp4/file" type="video/mp4">' +
+          '<source src="video/video' + producto.codigo + '.mp4" type="video/mp4">' +
           'Tu navegador no soporta video.' +
         '</video>' +
       '</div>'
