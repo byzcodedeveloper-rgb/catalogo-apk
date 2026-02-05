91551ebdcba138eec455333423b904750dec2d5a
byzcode <byzcode.developer@gmail.com>
2026-02-05 10:28:00 -0500

    Actualizacion de la vista 6

--- www/js/app.js
+++ www/js/app.js
@@ -17,18 +17,13 @@ $(document).on("pageinit", "#catalogo", function() {
   });
 });
 
-// ==============================
-// Vista de detalle con rotación táctil
-// ==============================
 $(document).on("click", "#lista-productos li a", function() {
   var codigo = $(this).data("id");
   $.getJSON("data/productos.json", function(productos) {
     var producto = productos.find(p => p.codigo === codigo);
     var detalle = $("#detalle-contenido");
-
-    // Renderizamos la imagen con ID para manipularla
     detalle.html(
-      '<img id="producto-img" src="' + producto.imagen + '" style="width:100%; max-width:300px; display:block; margin:auto; transform-style:preserve-3d;">' +
+      '<img src="' + producto.imagen + '" style="width:100%;">' +
       '<h2>' + producto.nombre + '</h2>' +
       '<p><b>Código:</b> ' + producto.codigo + '</p>' +
       '<p><b>W:</b> ' + producto.W + '</p>' +
@@ -36,28 +31,8 @@ $(document).on("click", "#lista-productos li a", function() {
       '<p><b>L:</b> ' + producto.L + '</p>' +
       '<p><b>Precio:</b> ' + producto.precio + '</p>'
     );
-
-    // ==============================
-    // Rotación manual con el dedo
-    // ==============================
-    const img = document.getElementById("producto-img");
-    let startX = 0;
-    let currentRotation = 0;
-
-    img.addEventListener("touchstart", function(e) {
-      startX = e.touches[0].clientX;
-    });
-
-    img.addEventListener("touchmove", function(e) {
-      let deltaX = e.touches[0].clientX - startX;
-      img.style.transform = `rotateY(${currentRotation + deltaX}deg)`;
-    });
-
-    img.addEventListener("touchend", function(e) {
-      let deltaX = e.changedTouches[0].clientX - startX;
-      currentRotation += deltaX;
-    });
   });
 });
+
 
 
